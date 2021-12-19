4c8ae969df18eb78d142c5422a1d82b4a2e66992
Nate Lovell <Natelovell99@gmail.com>
2021-12-18 21:02:32 -0700

    Create Photoshop Run Menu Items.jsx

---
+++ Photoshop Run Menu Items.jsx
@@ -0,0 +1,16 @@
+﻿// finding menuID
+
+// install ScriptingListener Plugin
+// https://helpx.adobe.com/photoshop/kb/downloadable-plugins-and-content.html
+
+// restart PS
+
+// open actions panel
+// press record in dummy action
+// "Insert Menu Item..."
+
+// open ~/Desktop/ScriptingListenerJS.log
+// find the stringIDToTypeID( "Your Action" );
+var idbrowseScripts = stringIDToTypeID( "browseScripts" );
+
+app.runMenuItem(idbrowseScripts);
